c334b15ba2631fa45031f48e43e591967ec03938
Ho Ming Jun <homingjun9@gmail.com>
2022-09-13 23:04:58 +0800

    Refactor Scripts
    1. Renamed GrenadeThrower to GrenadeThrowerManager
    2. Moved GrenadeThrowerManager and ShieldManager to the ActionManagers folder

--- Assets/Scripts/ActionManagers/GrenadeThrowerManager.cs
+++ Assets/Scripts/ActionManagers/GrenadeThrowerManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrenadeThrower : MonoBehaviour
+public class GrenadeThrowerManager : MonoBehaviour
 {
     [SerializeField]
     private Transform cam;
